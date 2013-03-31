fe2058bd39a8f484728278a6de95445cf7716dc7
Lawrence Kiss <larry@spothero.com>
2013-03-31 00:22:34 +0000

    Trying to get rid of deprecation warning.

--- coffin/conf/urls/defaults.py
+++ coffin/conf/urls/defaults.py
@@ -1,4 +1,5 @@
-from django.conf.urls import *
+from django.conf.urls import (handler403, handler404, handler500,
+        include, patterns, url)
 
 handler404 = 'coffin.views.defaults.page_not_found'
 handler500 = 'coffin.views.defaults.server_error'
